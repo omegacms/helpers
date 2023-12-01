c2375bfcc884ea0cdf6ce6215d8236558223ef10
omegacms <omegacms@outlook.com>
2023-12-01 20:47:26 +0100

    Adding system helper class

---
+++ src/System.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Part of Banco Omega CMS -  Helpers Package
+ *
+ * @link       https://omegacms.github.io
+ * @author     Adriano Giovannini <omegacms@outlook.com>
+ * @copyright  Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @license    https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ */
+
+/**
+ * @declare
+ */
+declare( strict_types = 1 );
+
+/**
+ * @namespace
+ */
+namespace Omega\Helpers;
+
+/**
+ * Systen helper class.
+ *
+ * @category    Omega
+ * @package     Omega\Helpers
+ * @link        https://omegacms.github.com
+ * @author      Adriano Giovannini <omegacms@outlook.com>
+ * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.com)
+ * @license     https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ * @version     1.0.0
+ */
+class System
+{
+    /**
+     * Get the operating system name.
+     *
+     * @return string Return the operating system name.
+     */
+    public static function getOperatingSystem() : string
+    {
+        $os = strtolower( PHP_OS_FAMILY );
+
+        switch ( $os ) {
+            case 'darwin':
+                return 'mac';
+            case 'win':
+                return 'windows';
+            case 'linux':
+                return 'linux';
+            default:
+                return 'unknown';
+        }
+    }
+}
