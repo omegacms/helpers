24d47a5eb73a64a688f4589d3eb6a6a24e6e2630
omegacms <omegacms@outlook.com>
2023-12-23 13:13:03 +0100

    Update Site Url

--- src/System.php
+++ src/System.php
@@ -25,9 +25,9 @@ namespace Omega\Helpers;
  *
  * @category    Omega
  * @package     Omega\Helpers
- * @link        https://omegacms.github.com
+ * @link        https://omegacms.github.io
  * @author      Adriano Giovannini <omegacms@outlook.com>
- * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.com)
+ * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
  * @license     https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  * @version     1.0.0
  */
